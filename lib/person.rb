85707e4566762046aa0d9b15004f70132af3eca5
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-18 19:13:52 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,25 @@
 # your code goes here
 class Person
-  attr_accessor :name, :bank_account, :happiness, :hygiene 
+  attr_accessor :name, :bank_account, :happiness, :hygiene
+
+  def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+  end
+
+  def clean?
+    self.hygiene > 7 ? true : false
+  end
+
+  def happy?
+    self.happiness > 7 ? true : false
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
 end
