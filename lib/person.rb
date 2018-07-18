50b12139ff7cabf285bb6426300ee12254dc9776
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-18 19:41:29 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -63,12 +63,12 @@ class Person
 
   def start_conversation(person, topic)
     if topic == 'politics'
-      self.happiness += 1
-      person.happiness -= 1
+      self.happiness += 2
+      person.happiness -= 2
       return "blah blah partisan blah lobbyist"
     elsif topic == 'weather'
-      self.happiness += 1
-      person.happiness += 1
+      self.happiness += 2
+      person.happiness += 2
       return "blah blah sun blah rain"
     else
       return "blah blah blah blah blah"
